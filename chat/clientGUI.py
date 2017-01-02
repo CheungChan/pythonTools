75dbc4d7eac13966e51a87ce59878bda9667f882
cheungchan <1377699408@qq.com>
2017-01-02 20:58:19 +0800

    chatv1.0

--- chat/clientGUI.py
+++ chat/clientGUI.py
@@ -5,10 +5,10 @@ from tkinter import messagebox
 from tkinter.colorchooser import *
 from tkinter.font import Font
 from functools import partial
-from datetime import datetime
+import re
 import os
 # import webbrowser
-import re
+import time
 import threading
 import socket
 import sys
@@ -21,7 +21,8 @@ class ChatGUI(object):
         self.SERVER_IP = '192.168.3.13'
         self.PORT = 21567
         self.ADDR = (self.SERVER_IP, self.PORT)
-        self.BUFSIZE = 1024 * 10
+        self.BUFSIZE = 1024 * 1024 * 10
+        self.historylist = []
 
         # 设置偏函数
         MyButton = partial(Button,bg='#BFEFFF', fg='black', activeforeground='#EFFAFF',activebackground='#6AD169', width= 10, height = 2)
@@ -31,16 +32,19 @@ class ChatGUI(object):
         self.top.geometry('700x480+50+80')
         self.top.resizable(False, False)
         # 增加菜单
-        self.addMenu()
+        menu = Menu(self.top)
+        menu.add_command(label="历史记录", command=self.check_history)
+        menu.add_command(label="选择颜色", command=self.choose_color)
+        self.top.config(menu=menu)
         # 消息框
         self.textfm = Frame(self.top)
         self.textsb_v =Scrollbar(self.textfm, orient=VERTICAL)
         self.text = Text(self.textfm, height=30, width=80, relief = 'flat', yscrollcommand=self.textsb_v.set)
         self.text.bind('<KeyPress>', lambda e:"break")
-        self.text.tag_configure('color', foreground='blue')
-        self.bold_font = Font(family="Consolas", size=11, weight="bold")
-        self.text.tag_configure("BOLD", font=self.bold_font)
-        self.text.tag_configure("system", font=self.bold_font, foreground='red')
+        self.text.tag_configure('name_time', foreground='blue')
+        self.message_font = Font(family="Consolas", size=11, weight="bold")
+        self.text.tag_configure("message", font=self.message_font)
+        self.text.tag_configure("system", font=self.message_font, foreground='red')
         self.text.pack(side=LEFT, fill=BOTH)
         self.textsb_v.pack(fill=Y,expand=0, side=LEFT, anchor=N)
         #在线人数
@@ -53,7 +57,7 @@ class ChatGUI(object):
         self.textfm.pack()
         # 输入框
         self.chatTextn = Text(self.top, width=80, height=5)
-        self.top.bind('<Return>', self.__out)
+        self.top.bind('<Return>', lambda e: self.__out())
         self.chatTextn.pack(side=LEFT)
         # 发送
         self.bfm = Frame(self.top)
@@ -70,6 +74,8 @@ class ChatGUI(object):
         data = json.dumps({"name": os.getlogin()})
         self.socket.send(data.encode())
         threading.Thread(target=self.__in).start()
+
+
     def __in(self):
         while True:
             try:
@@ -80,10 +86,23 @@ class ChatGUI(object):
             if not data:
                 print('没有从服务端接收到数据')
             data = json.loads(data)
+            name_time = data.get('name_time')
             message = data.get('message')
             online = data.get('online')
+            historylist = data.get('historylist')
+            history = data.get('history')
+            system = data.get('system')
+            if name_time:
+                self.insertText(name_time, 'name_time')
             if message:
-                self.insertText(message)
+                self.insertText(message,'message')
+            if system:
+                self.insertText(system, 'system')
+            if historylist:
+                self.historylist = historylist
+            if history:
+                self.text.delete('1.0', END)
+                self.insertText(history,'history')
             if online:
                 self.updateOnline(online)
 
@@ -92,8 +111,8 @@ class ChatGUI(object):
         data = self.getWholeTextMsg(self.chatTextn)
         if not data.strip():
             return
-        # if data.endswith('\n'):
-        #     data = data[:-1]
+        if data.endswith('\n'):
+            data = data[:-1]
         data = json.dumps({"message":data})
         self.socket.send(data.encode())
         self.chatTextn.delete('1.0', 'end')
@@ -110,13 +129,6 @@ class ChatGUI(object):
         return text.get('1.0','end-1c')
 
 
-    def addMenu(self):
-        menu = Menu(self.top)
-        menu.add_command(label="历史记录", command=self.check_history)
-        menu.add_command(label="选择颜色", command=self.choose_color)
-        self.top.config(menu=menu)
-
-
     def check_history(self):
 
         # webbrowser.open("http://localhost:8000")
@@ -124,8 +136,12 @@ class ChatGUI(object):
         self.tl.focus()
         self.tl.title("选择历史纪录日期")
         MODES = []
-        
-        for f in os.listdir("D://gitdir/pythonTools/chat/history"):
+
+        data = json.dumps({"history":"?"})
+        self.socket.send(data.encode())
+        while not self.historylist:
+            time.sleep(0.1)
+        for f in self.historylist:
             # 文件名.db去掉.db来显示
             fl = len(f[:-3])
             fv = f[:-3] + " " * (60 - fl)
@@ -145,25 +161,32 @@ class ChatGUI(object):
     def selected(self):
 
         self.tl.destroy()
-        self.text.delete('1.0', 'end')
-        self.insertText(self.v.get())
+        history = self.v.get()
+        data = json.dumps({"history": history})
+        self.socket.send(data.encode())
 
 
-    def insertText(self, message):
-        for _l in message.split('\n'):
-            if _l.startswith('【系统提示】'):
-                self.text.insert(END, _l + '\n','system')
-            elif re.match('【.*】：\s+\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}',_l):
-                self.text.insert(END, '\n' + _l + '\n', 'color')
-            else:
-                self.text.insert(END, _l, 'BOLD')
+    def insertText(self, data, type):
+
+        if type == 'history':
+            l = data.split('\n')
+            self.text.insert(END, '历史记录：\n', 'system')
+            for _l in l:
+                if re.match('^【.*】：\s+\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}$', _l):
+                    self.text.insert(END, _l + '\n', 'name_time')
+                elif _l.startswith('【系统提示】'):
+                    self.text.insert(END, _l + '\n', 'system')
+                else:
+                    self.text.insert(END, _l + '\n', 'message')
+        else:
+            self.text.insert(END, data, type)
         self.text.see(END)
 
 
     def choose_color(self):
 
         color = askcolor()
-        self.text.tag_configure("BOLD", font=self.bold_font,foreground=color[1])
+        self.text.tag_configure("message", font=self.message_font,foreground=color[1])
 
 
 if __name__ == '__main__':
--- chat/server.py
+++ chat/server.py
@@ -5,6 +5,8 @@ import threading
 import socket
 from datetime import datetime
 import json
+import os
+import os.path
 
 class server:
 
@@ -23,41 +25,65 @@ class server:
             conn, addr = self.socket.accept()
             threading.Thread(target=self.__in, args=(conn,)).start()
 
-    def __in(self,conn):
+    def __in(self, conn):
         while True:
             try:
                 tmp = json.loads(conn.recv(self.BUFSIZE).decode())
             except (ConnectionRefusedError,ConnectionResetError):
                 name = self.clients[conn]
                 del self.clients[conn]
-                self.__printAndSendMsg('【系统提示】' + name + "下线了")
+                self.__printAndSendMsg('【系统提示】' + name + "下线了" + '\n', 'system')
                 return
             if not tmp:
                 print("服务端没有接收到数据")
             name = tmp.get('name')
             data = tmp.get('message')
+            history = tmp.get('history')
             if name:
                 self.clients[conn] = name
-                self.__printAndSendMsg('【系统提示】' + name + "上线了")
-                continue
+                self.__printAndSendMsg('【系统提示】' + name + "上线了" + '\n', 'system')
             elif data:
-                data = '【' + self.clients[conn] + '】：    ' + datetime.now().strftime('%Y/%m/%d %X') + \
-                      '\n' + data
-                self.__printAndSendMsg(data)
+                self.__printAndSendMsg('【' + self.clients[conn] + '】：    ' + datetime.now().strftime(
+                    '%Y/%m/%d %X') + '\n','name_time')
+                self.__printAndSendMsg(data + '\n', 'message')
+            elif history:
+                if history == '?':
+                    l = json.dumps({"historylist": os.listdir(os.path.join(os.getcwd(),'history'))})
+                    conn.send(l.encode())
+                else:
+                    with open(os.path.join(os.getcwd(), 'history', history), encoding='utf-8') as f:
+                        self.__sendhistory(f.read(), conn)
 
 
-    def __printAndSendMsg(self, data):
+    def __printAndSendMsg(self, data, type):
 
         print(data)
+        with open(os.path.join('history', datetime.now().strftime('%Y%m%d %HXXXX')+".db"), 'a',
+                  encoding='utf-8') \
+                as f:
+            f.write(data)
         online = ""
         for name in self.clients.values():
             online += name + '\n'
-        message = json.dumps({'message': data, "online": online})
+        message = json.dumps({type: data, "online": online})
         for c in self.clients.keys():
             try:
                 c.send(message.encode())
             except (ConnectionResetError, ConnectionAbortedError):
                 print(c,'发送失败')
+
+
+    def __sendhistory(self, data, conn):
+        print('发送给【' + self.clients[conn] + '】历史记录')
+        online = ""
+        for name in self.clients.values():
+            online += name + '\n'
+        message = json.dumps({"history": data, "online": online})
+        try:
+            conn.send(message.encode())
+        except:
+            print(conn, '发送失败')
+
 
 if __name__ == '__main__':
     server()
