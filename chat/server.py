a50cb67879209acec28586ee4e2a6b3e6ed7e029
香饽饽 <1377699408@qq.com>
2018-05-26 13:46:18 +0800

    设置端口重用

--- chat/server.py
+++ chat/server.py
@@ -24,6 +24,7 @@ class server:
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(self.ADDR)
+        self.socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
         self.socket.listen(10)
         print('服务端启动了' + self.get_format_now())
         flag = False
